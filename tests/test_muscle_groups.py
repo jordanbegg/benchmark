e2763a4ddfcb3bc28487b13a38b4f018e6bdc296
Jordan Begg <Jordan.Begg@woodplc.com>
2023-04-16 15:46:08 +0100

    refactors tests

--- tests/test_muscle_groups.py
+++ tests/test_muscle_groups.py
@@ -1,10 +1,11 @@
 from fastapi.testclient import TestClient
 
 from .fixtures import client_fixture, session_fixture
+from .utils import create_muscle_group
 
 
 def test_create_muscle_group(client: TestClient):
-    response = client.post("/musclegroups/", json={"name": "Chest"})
+    response = create_muscle_group(client=client)
     data = response.json()
 
     assert response.status_code == 200
@@ -14,71 +15,49 @@ def test_create_muscle_group(client: TestClient):
 
 def test_read_muscle_group(client: TestClient):
     # sourcery skip: extract-duplicate-method
-    response = client.post("/musclegroups/", json={"name": "Chest"})
+    response = create_muscle_group(client=client)
     data = response.json()
 
-    assert response.status_code == 200
-    assert data["name"] == "Chest"
-    assert data["id"] is not None
-
-    created_id = data["id"]
-    response = client.get(f"/musclegroups/{created_id}")
+    response = client.get("/musclegroups/1")
     data = response.json()
     assert response.status_code == 200
     assert data["name"] == "Chest"
-    assert data["id"] == created_id
+    assert data["id"] == 1
     assert data["exercises"] == []
 
 
 def test_read_muscle_groups(client: TestClient):
-    response = client.post("/musclegroups/", json={"name": "Chest"})
+    response = create_muscle_group(client=client)
     data = response.json()
 
-    assert response.status_code == 200
-    assert data["name"] == "Chest"
-    assert data["id"] is not None
-
-    created_id = data["id"]
     response = client.get("/musclegroups/")
     data = response.json()
     assert response.status_code == 200
     assert len(data) == 1
     assert data[0]["name"] == "Chest"
-    assert data[0]["id"] == created_id
+    assert data[0]["id"] == 1
 
 
 def test_delete_muscle_group(client: TestClient):
-    response = client.post("/musclegroups/", json={"name": "Chest"})
+    response = create_muscle_group(client=client)
     data = response.json()
 
-    assert response.status_code == 200
-    assert data["name"] == "Chest"
-    assert data["id"] is not None
-
-    created_id = data["id"]
-    response = client.delete(f"/musclegroups/{created_id}")
+    response = client.delete("/musclegroups/1")
     data = response.json()
     assert response.status_code == 200
 
-    response = client.get(f"/musclegroups/{created_id}")
+    response = client.get("/musclegroups/1")
     data = response.json()
     assert response.status_code == 404
 
 
 def test_update_muscle_group(client: TestClient):
     # sourcery skip: extract-duplicate-method
-    response = client.post("/musclegroups/", json={"name": "chest"})
+    response = create_muscle_group(client=client)
     data = response.json()
 
+    response = client.patch("/musclegroups/1", json={"name": "chest"})
+    data = response.json()
     assert response.status_code == 200
     assert data["name"] == "chest"
-    assert data["id"] is not None
-
-    created_id = data["id"]
-    response = client.patch(
-        f"/musclegroups/{created_id}", json={"name": "Chest"}
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Chest"
-    assert data["id"] == created_id
+    assert data["id"] == 1
--- tests/test_workouts.py
+++ tests/test_workouts.py
@@ -1,363 +1,90 @@
 from fastapi.testclient import TestClient
 
 from .fixtures import client_fixture, session_fixture
+from .utils import create_workout, create_workout_routine, create_exercise
 
 
 def test_create_workout(client: TestClient):
-    # Create an exercise
-    # sourcery skip: extract-duplicate-method
-    response = client.post(
-        "/exercises/",
-        json={"name": "Bench Press"},
-    )
+    response = create_workout(client=client)
     data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Bench Press"
-    assert data["id"] is not None
 
-    # Create a workout routine
-    exercise_id = data["id"]
-    payload = {
-        "name": "Test Routine",
-        "exercises": [{"id": exercise_id, "planned_sets": [{"reps": 4}]}],
-    }
-    response = client.post(
-        "/workout_routines/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Test Routine"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["planned_sets"][0]["reps"] == 4
-
-    # Create a workout
-    workout_routine_id = data["id"]
-    payload = {
-        "date": "1990-01-01",
-        "exercises": [
-            {"id": exercise_id, "sets": [{"reps": 4, "weight": 60}]}
-        ],
-        "workoutroutine_id": workout_routine_id,
-    }
-    response = client.post(
-        "/workouts/",
-        json=payload,
-    )
-    data = response.json()
     assert response.status_code == 200
     assert data["date"] == "1990-01-01"
     assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
+    assert data["exercises"][0]["id"] == 1
     assert data["exercises"][0]["sets"][0]["weight"] == 60
-    assert data["workoutroutine_id"] == workout_routine_id
+    assert data["workoutroutine_id"] == 1
 
 
 def test_create_workout_with_bad_exercise(client: TestClient):
     # Create an exercise
     # sourcery skip: extract-duplicate-method
-    response = client.post(
-        "/exercises/",
-        json={"name": "Bench Press"},
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Bench Press"
-    assert data["id"] is not None
+    response = create_workout_routine(client=client)
+    response = client.post("/exercises/", json={"name": "bad"})
 
-    # Create an exercise
-    # sourcery skip: extract-duplicate-method
-    response = client.post(
-        "/exercises/",
-        json={"name": "Triceps"},
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Triceps"
-    assert data["id"] is not None
-
-    # Create a workout routine
-    exercise_id = data["id"]
-    payload = {
-        "name": "Test Routine",
-        "exercises": [{"id": exercise_id, "planned_sets": [{"reps": 4}]}],
-    }
-    response = client.post(
-        "/workout_routines/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Test Routine"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["planned_sets"][0]["reps"] == 4
-
-    # Create a workout
-    workout_routine_id = data["id"]
-    payload = {
-        "date": "1990-01-01",
-        "exercises": [{"id": 1, "sets": [{"reps": 4, "weight": 60}]}],
-        "workoutroutine_id": workout_routine_id,
-    }
     response = client.post(
         "/workouts/",
-        json=payload,
+        json={
+            "date": "1990-01-01",
+            "exercises": [{"id": 2, "sets": [{"reps": 4, "weight": 60}]}],
+            "workoutroutine_id": 1,
+        },
     )
-    data = response.json()
     assert response.status_code == 404
 
 
 def test_read_workout(client: TestClient):
-    # Create an exercise
-    # sourcery skip: extract-duplicate-method
-    response = client.post(
-        "/exercises/",
-        json={"name": "Bench Press"},
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Bench Press"
-    assert data["id"] is not None
-
-    # Create a workout routine
-    exercise_id = data["id"]
-    payload = {
-        "name": "Test Routine",
-        "exercises": [{"id": exercise_id, "planned_sets": [{"reps": 4}]}],
-    }
-    response = client.post(
-        "/workout_routines/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Test Routine"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["planned_sets"][0]["reps"] == 4
-
-    # Create a workout
-    workout_routine_id = data["id"]
-    payload = {
-        "date": "1990-01-01",
-        "exercises": [
-            {"id": exercise_id, "sets": [{"reps": 4, "weight": 60}]}
-        ],
-        "workoutroutine_id": workout_routine_id,
-    }
-    response = client.post(
-        "/workouts/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["date"] == "1990-01-01"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["sets"][0]["weight"] == 60
-    assert data["workoutroutine_id"] == workout_routine_id
+    response = create_workout(client=client)
 
     # Read the workout
-    created_id = data["id"]
-    response = client.get(f"/workouts/{created_id}")
+    response = client.get("/workouts/1")
     data = response.json()
     assert response.status_code == 200
     assert data["date"] == "1990-01-01"
-    assert data["id"] == created_id
-    assert data["exercises"][0]["id"] == exercise_id
+    assert data["id"] == 1
+    assert data["exercises"][0]["id"] == 1
     assert data["exercises"][0]["sets"][0]["weight"] == 60
 
 
 def test_read_workouts(client: TestClient):
-    # Create an exercise
-    # sourcery skip: extract-duplicate-method
-    response = client.post(
-        "/exercises/",
-        json={"name": "Bench Press"},
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Bench Press"
-    assert data["id"] is not None
+    response = create_workout(client=client)
 
-    # Create a workout routine
-    exercise_id = data["id"]
-    payload = {
-        "name": "Test Routine",
-        "exercises": [{"id": exercise_id, "planned_sets": [{"reps": 4}]}],
-    }
-    response = client.post(
-        "/workout_routines/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Test Routine"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["planned_sets"][0]["reps"] == 4
-
-    # Create a workout
-    workout_routine_id = data["id"]
-    payload = {
-        "date": "1990-01-01",
-        "exercises": [
-            {"id": exercise_id, "sets": [{"reps": 4, "weight": 60}]}
-        ],
-        "workoutroutine_id": workout_routine_id,
-    }
-    response = client.post(
-        "/workouts/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["date"] == "1990-01-01"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["sets"][0]["weight"] == 60
-    assert data["workoutroutine_id"] == workout_routine_id
-
-    created_id = data["id"]
     response = client.get("/workouts/")
     data = response.json()
     assert response.status_code == 200
     assert len(data) == 1
     assert data[0]["date"] == "1990-01-01"
-    assert data[0]["id"] == created_id
+    assert data[0]["id"] == 1
 
 
 def test_delete_workout(client: TestClient):
-    # Create an exercise
-    # sourcery skip: extract-duplicate-method
-    response = client.post(
-        "/exercises/",
-        json={"name": "Bench Press"},
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Bench Press"
-    assert data["id"] is not None
+    response = create_workout(client=client)
 
-    # Create a workout routine
-    exercise_id = data["id"]
-    payload = {
-        "name": "Test Routine",
-        "exercises": [{"id": exercise_id, "planned_sets": [{"reps": 4}]}],
-    }
-    response = client.post(
-        "/workout_routines/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Test Routine"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["planned_sets"][0]["reps"] == 4
-
-    # Create a workout
-    workout_routine_id = data["id"]
-    payload = {
-        "date": "1990-01-01",
-        "exercises": [
-            {"id": exercise_id, "sets": [{"reps": 4, "weight": 60}]}
-        ],
-        "workoutroutine_id": workout_routine_id,
-    }
-    response = client.post(
-        "/workouts/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["date"] == "1990-01-01"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["sets"][0]["weight"] == 60
-    assert data["workoutroutine_id"] == workout_routine_id
-
-    created_id = data["id"]
-    response = client.delete(f"/workouts/{created_id}")
-    data = response.json()
+    response = client.delete("/workouts/1")
     assert response.status_code == 200
 
     # TODO Check the sets were deleted
 
     # Check the workout was deleted
-    response = client.get(f"/workouts/{created_id}")
-    data = response.json()
+    response = client.get("/workouts/1")
     assert response.status_code == 404
 
 
 def test_update_workout(client: TestClient):
-    # Create an exercise
-    # sourcery skip: extract-duplicate-method
-    response = client.post(
-        "/exercises/",
-        json={"name": "Bench Press"},
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Bench Press"
-    assert data["id"] is not None
-
-    # Create a workout routine
-    exercise_id = data["id"]
-    payload = {
-        "name": "Test Routine",
-        "exercises": [{"id": exercise_id, "planned_sets": [{"reps": 4}]}],
-    }
-    response = client.post(
-        "/workout_routines/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["name"] == "Test Routine"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["planned_sets"][0]["reps"] == 4
-
-    # Create a workout
-    workout_routine_id = data["id"]
-    payload = {
-        "date": "1990-01-01",
-        "exercises": [
-            {"id": exercise_id, "sets": [{"reps": 4, "weight": 60}]}
-        ],
-        "workoutroutine_id": workout_routine_id,
-    }
-    response = client.post(
-        "/workouts/",
-        json=payload,
-    )
-    data = response.json()
-    assert response.status_code == 200
-    assert data["date"] == "1990-01-01"
-    assert data["id"] is not None
-    assert data["exercises"][0]["id"] == exercise_id
-    assert data["exercises"][0]["sets"][0]["weight"] == 60
-    assert data["workoutroutine_id"] == workout_routine_id
-
-    workout_id = data["id"]
+    response = create_workout(client=client)
 
     payload = {
         "date": "1990-01-02",
-        "exercises": [
-            {"id": exercise_id, "sets": [{"reps": 2, "weight": 1000}]}
-        ],
+        "exercises": [{"id": 1, "sets": [{"reps": 2, "weight": 1000}]}],
     }
     response = client.patch(
-        f"/workouts/{workout_id}",
+        "/workouts/1",
         json=payload,
     )
     data = response.json()
     assert response.status_code == 200
     assert data["date"] == "1990-01-02"
-    assert data["id"] == workout_id
+    assert data["id"] == 1
     assert data["exercises"][0]["name"] == "Bench Press"
     assert data["exercises"][0]["sets"][0]["reps"] == 2
     assert data["exercises"][0]["sets"][0]["weight"] == 1000
